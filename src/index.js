50792e685bccd043bf1fb5f9cf99c926a3f428a6
Manya Sharma <manya.sharma395@gmail.com>
2026-01-30 21:48:14 +0530

    Build index.js entry point, Add code to store menu data

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContact from "./contact";
+
+function clearContent() {
+    const content = document.getElementById('content');
+    content.textContent = '';
+}
+
+document.getElementById('home-button').addEventListener('click', () => {
+    clearContent;
+    loadHome();
+});
+
+document.getElementById('menu-button').addEventListener('click', () => {
+    clearContent;
+    loadMenu();
+});
+
+document.getElementById('contact-button').addEventListener('click', () => {
+    clearContent;
+    loadContact();
+});
+
+loadHome(); //default page
--- src/menu.js
+++ src/menu.js
@@ -36,7 +36,55 @@ function createMenuCard(item) {
     card.appendChild(desc);
 }
 
-//store menu data clearly
+//store menu data 
+const beveragesData = [
+    {
+        name: 'Masala Chai',
+        desc: 'Traditional Indian tea brewed with aromatic spices.',
+        img:'https://goqii.com/blog/wp-content/uploads/shutterstock_1024718095-1024x682.jpg'
+    },
+    {
+        name: 'Fresh Lime Soda (Sweet / Salted)',
+        desc: 'A refreshing blend of lime and sparkling soda.',
+        img: 'https://sattvakitchen.com/wp-content/uploads/2024/05/SWEET-LIME-SODA-shutterstock_2309599743-copy-Copy-copy.jpg'
+    }
+];
+
+const sidesData = [
+    {
+        name: 'Paneer Pakora',
+        desc: 'Soft cottage cheese fritters, lightly spiced and fried',
+        img: 'https://images.slurrp.com/prod/recipe_images/transcribe/snack/Crispy-paneer-pakora.webp'
+    },
+    {
+        name: 'Vegetable Samosa',
+        desc: 'Crisp pastry filled with spiced potatoes and peas',
+        img: 'https://vegecravings.com/wp-content/uploads/2017/03/Aloo-Samosa-Recipe-Step-By-Step-Instructions.jpg'
+    }
+];
+
+const mainsData = [
+    {
+        name: 'Dal Makhani',
+        desc: 'Slow-cooked black lentils finished with butter and cream',
+        img: 'https://vegecravings.com/wp-content/uploads/2016/03/Dal-Makhani-Recipe-Step-By-Step-Instructions.jpg'
+    },
+    {
+        name: 'Shahi Paneer',
+        desc: 'A royal curry made with cashew gravy and aromatic spices',
+        img: 'https://maayeka.com/wp-content/uploads/2020/09/shahi-paneer-recipe.jpg'
+    },
+    {
+        name: 'Vegetable Biryani',
+        desc: 'Fragrant basmati rice layered with vegetables and whole spices',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtDiMiok2ektyhL9ZFNhPX3psPNJKIaVY3Ng&s'
+    },
+    {
+        name: 'Malai Kofta',
+        desc: 'Soft vegetable dumplings served in a creamy, delicately spiced sauce',
+        img: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/04/malai-kofta-2.jpg'
+    }
+]
 
 
 function loadMenu() {
