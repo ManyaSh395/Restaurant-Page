cecc76fb615edfc78fd68c12f0fd8fd22773d272
Manya Sharma <manya.sharma395@gmail.com>
2026-02-03 21:50:07 +0530

    Add menu items in munu file, Add basic styles for menu page

--- src/menu.js
+++ src/menu.js
@@ -41,12 +41,12 @@ const beveragesData = [
     {
         name: 'Masala Chai',
         desc: 'Traditional Indian tea brewed with aromatic spices.',
-        img:'https://goqii.com/blog/wp-content/uploads/shutterstock_1024718095-1024x682.jpg'
+        img:'https://media.istockphoto.com/id/1336601313/photo/top-view-of-indian-herbal-masala-chai-or-traditional-beverage-tea-with-milk-and-spices-kerala.jpg?s=612x612&w=is&k=20&c=lupk7Uqlsk3q_PSEoUoCMhSmHe6H_E9ePJ3knAqhLqk='
     },
     {
         name: 'Fresh Lime Soda (Sweet / Salted)',
         desc: 'A refreshing blend of lime and sparkling soda.',
-        img: 'https://sattvakitchen.com/wp-content/uploads/2024/05/SWEET-LIME-SODA-shutterstock_2309599743-copy-Copy-copy.jpg'
+        img: 'https://media.istockphoto.com/id/1482593716/photo/sparkling-cold-water-with-lime-and-ice.jpg?s=612x612&w=is&k=20&c=3aDaxr8EgJfcl5ds2mzTpc2FGBO2npshM8Jwq9rh030='
     }
 ];
 
@@ -54,12 +54,12 @@ const sidesData = [
     {
         name: 'Paneer Pakora',
         desc: 'Soft cottage cheese fritters, lightly spiced and fried',
-        img: 'https://images.slurrp.com/prod/recipe_images/transcribe/snack/Crispy-paneer-pakora.webp'
+        img: 'https://media.istockphoto.com/id/1272417152/photo/cottage-cheese-cutlet.jpg?s=612x612&w=is&k=20&c=djD6RxYgrs6IqDgXVWHMDT7vAEIS9Fps8gf0mIBkVZc='
     },
     {
         name: 'Vegetable Samosa',
         desc: 'Crisp pastry filled with spiced potatoes and peas',
-        img: 'https://vegecravings.com/wp-content/uploads/2017/03/Aloo-Samosa-Recipe-Step-By-Step-Instructions.jpg'
+        img: 'https://media.istockphoto.com/id/1299380316/photo/samosa-with-chutney-in-plate-asian-breakfast-aloo-samosa.jpg?s=612x612&w=is&k=20&c=5IiOHnjAiCgCEaxLA5jOqRv2yEX7DaajGyNuSIVKO6c='
     }
 ];
 
@@ -67,24 +67,37 @@ const mainsData = [
     {
         name: 'Dal Makhani',
         desc: 'Slow-cooked black lentils finished with butter and cream',
-        img: 'https://vegecravings.com/wp-content/uploads/2016/03/Dal-Makhani-Recipe-Step-By-Step-Instructions.jpg'
+        img: 'https://media.istockphoto.com/id/1170374719/photo/dal-makhani-at-dark-background.jpg?s=612x612&w=is&k=20&c=QJshmbqDy8vLyrjUHN5grDvOK7-OpuczfM13W6VrPhg='
     },
     {
         name: 'Shahi Paneer',
         desc: 'A royal curry made with cashew gravy and aromatic spices',
-        img: 'https://maayeka.com/wp-content/uploads/2020/09/shahi-paneer-recipe.jpg'
+        img: 'https://media.istockphoto.com/id/178899560/photo/paneer-butter-masala-in-metal-bowl.jpg?s=1024x1024&w=is&k=20&c=y14kKnQPE32WEBKG9UzsWrQwhQbENpOCQc1aLli-kws='
     },
     {
         name: 'Vegetable Biryani',
         desc: 'Fragrant basmati rice layered with vegetables and whole spices',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtDiMiok2ektyhL9ZFNhPX3psPNJKIaVY3Ng&s'
+        img: 'https://media.istockphoto.com/id/1292442851/photo/traditional-hyderabadi-vegetable-veg-dum-biryani-with-mixed-veggies-served-with-mixed-raita.jpg?s=1024x1024&w=is&k=20&c=1iHhe6Yz3rRW5ItvlD6iRVXR-QTOldg4LGaA38NZO3w='
     },
     {
         name: 'Malai Kofta',
         desc: 'Soft vegetable dumplings served in a creamy, delicately spiced sauce',
-        img: 'https://www.vegrecipesofindia.com/wp-content/uploads/2021/04/malai-kofta-2.jpg'
+        img: 'https://media.istockphoto.com/id/1219174099/photo/malai-kofta-curry-in-black-bowl-at-dark-slate-background-malai-kofta-is-indian-cuisine-dish.jpg?s=1024x1024&w=is&k=20&c=YHeHZY9ziVitHOAMs4m1-ri2ow9DpBkOyirs-sLagI0='
     }
 ]
+
+const dessertsData = [
+    {
+        name: 'Gulab Jamun',
+        desc: 'Soft milk-solid dumplings soaked in warm rose-flavoured sugar syrup',
+        img: 'https://media.istockphoto.com/id/1188000786/photo/gulab-jamun-in-bowl-on-wooden-background-indian-dessert-or-sweet-dish.jpg?s=1024x1024&w=is&k=20&c=p92iX5UnAJgkFWt4jp8S-fRiNkBWnooA-eEUqdDNG2E='
+    },
+    {
+        name: 'Kesar Rasmallai',
+        desc: 'Delicate cottage cheese discs served in saffron-infused, sweetened milk',
+        img: 'https://media.istockphoto.com/id/2203259296/photo/deliciously-soft-rasmalai-soaked-in-saffron-infused-sweetened-milk-with-nutty-almonds-and.jpg?s=1024x1024&w=is&k=20&c=gX6fC3DltnfeD723FhRLdwcFKpRU2ZPxsAJJQhlkV5Y='
+    }
+];
 
 
 function loadMenu() {
